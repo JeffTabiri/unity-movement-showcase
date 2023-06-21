a643f17001c8c2c7c53bad10f9843526886ed21c
tabir <tabirijeffrey@gmail.com>
2023-06-21 20:52:46 +0200

    feat(playerJump): add isGrounded requirement to the players jump

--- PlayerJump.cs
+++ PlayerJump.cs
@@ -6,17 +6,19 @@ using UnityEngine.InputSystem;
 public class PlayerJump : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private CollisionChecker collisionChecker;
     private float jumpMaxHeight = 10f;
     
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        collisionChecker = GetComponent<CollisionChecker>();
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.started && collisionChecker.isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpMaxHeight);
         }
