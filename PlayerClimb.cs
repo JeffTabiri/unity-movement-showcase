5d2b535618b50712ed740200b8a2f13f2a37ec2a
tabir <tabirijeffrey@gmail.com>
2023-07-06 03:57:11 +0200

    feat(playerClimb): add wallslide for player

--- PlayerClimb.cs
+++ PlayerClimb.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,9 +9,17 @@ public class PlayerClimb : MonoBehaviour
     //Necessary Components
     private Rigidbody2D rb;
     private CollisionChecker collisionChecker;
+    
+    //Walljump components
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
 
-    [SerializeField]
-    private bool isWallSliding = false;
+    private bool speedDeclined = false;
+
     
     // Start is called before the first frame update
     void Awake()
@@ -19,11 +28,35 @@ public class PlayerClimb : MonoBehaviour
         collisionChecker = GetComponent<CollisionChecker>();
     }
 
-
-    public void WallSlide()
+    private void Update()
     {
-        if (!collisionChecker.isGrounded && collisionChecker.isWalled) ;
-            isWallSliding = true;
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -40, float.MaxValue));
+        WallSlide();
+    }
+
+    private void WallSlide()
+    {
+        if (collisionChecker.isWalled && !collisionChecker.isGrounded && rb.velocity.x != 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -0.01f, float.MaxValue));
         }
     }
+
+    private void WallJump()
+    {
+        if (collisionChecker.isWalled)
+        {
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+        }
+    }
+}
